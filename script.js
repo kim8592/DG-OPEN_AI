3125c7674cc4b90f77476ad8ee75f05ec0d39415
kim8592 <kimlankhanhbinh@gmail.com>
2026-04-15 07:46:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -779,7 +779,7 @@ Hãy viết nhận xét cho từng học sinh theo đúng ID và trả về đú
       { role: "system", content: systemPrompt },
       { role: "user", content: userInstruction }
     ],
-    temperature: 0.7
+    temperature: 0.8
   })
 });
 
